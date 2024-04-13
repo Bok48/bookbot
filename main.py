6eab44dd1f90e6ef9fdbd528d9ab76c28cd713a9
Bok48 <janolav.aspelund@lyse.net>
2024-04-13 02:43:47 +0200

    mhå mvp done

---
+++ main.py
@@ -0,0 +1,35 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        book = f.read()
+        letters = get_letter_count(book)
+        print(get_word_count(book))
+        for letter in letters:
+            print(letter)
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+
+def get_letter_count(book):
+    book_low = book.lower()
+    letter_counts = {}
+    for letter in book_low:
+        if letter in letter_counts:
+            letter_counts[letter] += 1
+        elif letter.isalpha():
+            letter_counts[letter] = 1
+    return convert_to_sorted_dic_ls(letter_counts)
+
+def convert_to_sorted_dic_ls(dic):
+    ls = []
+    for letter in dic:
+        entry = {"letter": letter, "count": dic[letter]}
+        ls.append(entry)
+    ls.sort(reverse=True, key=get_count)
+    return ls
+
+def get_count(letter):
+    return letter["count"]
+
+main()
