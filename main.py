6a3f95115134be13c1c5ab0b92ddab11fd85cbb2
Bok48 <janolav.aspelund@gmail.com>
2025-09-20 01:30:35 +0200

    change: arguments as paths for books

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from stats import (count_words,
                    count_characters, 
                    sort_characters)
+import sys
 
 def get_book_text(book):
-    with open(f"books/{book}") as f:
+    with open(book) as f:
         book_text = f.read()
     return book_text
 
 def main():
-    book = "frankenstein.txt" # Name of text that is being analyzed
-                              # placed in books folder
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book = sys.argv[1] # Path of text that is being analyzed,
+                       # placed in books folder
 
     book_text = get_book_text(book)
     num_words = count_words(book_text)
@@ -20,11 +25,12 @@ def main():
 
     # Start of printout
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/{book}...")
+    print(f"Analyzing book found at {book}...")
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
     
+    # Printing all alphabetical characters in order of most to least used
     for char_dict in sorted_chars:
         if char_dict["char"].isalpha():
             print(f"{char_dict["char"]}: {char_dict["num"]}")
