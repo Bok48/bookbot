f46cc0068c1020109077a42e5630325a2f6312b1
Bok48 <janolav.aspelund@gmail.com>
2025-09-19 12:30:54 +0200

    add get_book_text and main functions

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(book):
+    with open(f"books/{book}") as f:
+        book_text = f.read()
+    return book_text
+
+def main():
+    book = get_book_text("frankenstein.txt")
+    print(f"{book}")
+
+main()
