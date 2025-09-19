40ce8f9e8e46e43c6d3725888025ce33d3bee071
Bok48 <janolav.aspelund@gmail.com>
2025-09-19 12:49:31 +0200

    move count_words function into stats.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import count_words
+
 def get_book_text(book):
     with open(f"books/{book}") as f:
         book_text = f.read()
     return book_text
-
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def main():
     book = get_book_text("frankenstein.txt")
